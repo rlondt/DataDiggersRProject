092b6324015c50e03a06a3a6d62a3c9f47489a73
rlondt <rlondt@minjak.nl>
2019-10-15 22:20:31 +0200

    Operationalisatie datakwaliteit toegevoegd.

--- data-quality.R
+++ data-quality.R
@@ -5,6 +5,70 @@ if (file.exists('./init.R')){
 ordersWorkflowDF <- read_rds("ordersWorkflowDF.rds")
 ordersTijdschrijvenDF <- read_rds("ordersTijdschrijvenDF.rds")
 
+
+
+# Compleetheid
+# 1. Mogelijkheden voor kruistellingen
+# 2. Zijn alle onderdelen van VWT-data vertegenwoordigd
+# 3. Missing values analyse 
+# 4. Overcompleetheid, dataoverload
+# 5. Komen alle features voor die nodig zijn om een analyse te doen
+# ..
+# Consistentie
+# 1. Komt elke ordernummer van het tijdschrijven voor in de workflowDF, en vice versa
+# 2. Alle relaties twee kanten op
+#   a. order zonder workflow
+#   b. workflow zonder order
+#   c. tijdschrijven zonder medewerker
+#   d. medewerker zonder tijdschrijven
+#   e. tijdschrijven zonder dienst
+#   f. dienst zonder tijdschrijven
+#   g. tijdschrijven zonder order
+#   h. order zonder tijdschrijven
+# 3. Is bij tijdschrijven, de begintijd altijd kleiner dan de eindtijd
+# 4. Bevinden de begintijd en eindtijd van het tijdschrijven zich binnen de begintijd en eindtijd van het rooster
+# 5. De relatie tussen rooster en medewerker en tijdschrijven en medewerker loopt "dubbel". Komen daar inconsistenties in voor
+# 6. Datumvelden als datum te behandelen ⇒ OK
+# 7. Zijn de order oplopend
+# 8. Naamgeving kolommen
+# 9. Controle postcode-plaatsnaam zowel voor persoon als order
+# ..
+# Uniqueness
+# 1. Geen dubbele records
+# 2. Zijn er medewerkers die tegelijkertijd aan meerdere orders werken ?
+# 3. Zijn er medewerkers die tegelijkertijd voor meerdere orders reizen?
+# 4. Overlappen de workflowstappen (kan zijn hoor)
+# ..
+# Validiteit (plausibiliteit/business rules)
+# 1. Is er daarbinnen nog verschil tussen werk en reistijd? Hoe gaan we om met overwerk?
+# 2. Vervallen orders waar wel geakkoordeerd is tijdgeschreven
+# 3. Anomaly detection/outlier verklaring.. 
+# 4. Inventarisatie business rules
+# 5. Heeft medewerker altijd een woonplaats
+# 6. Heeft elke order een plaats
+# 7. Orders die binnen een periode 'x' (vb een minuut) zijn uitgevoerd
+# 8. Verdeling van de reistijd over de medewerkers
+# 9. Aantal tijdschrijvers per order
+# 10. Verhouding aantal tijdschrijvers tov normtijd (outlier??)
+# 11. Tijdschrijvers ten opzichte van het order-klantteam (klantenteams VWT NOC geen tijdschrijvers??)
+# 
+# 
+# 
+# Accuraatheid
+# 1. Verdeling aantal orders per over de tijd uitgezet.(Heatmap timeline )
+# 
+# Analyses voor normtijden
+# 1. Reistijd tussen verschillende orders met zelfde plaats 
+# 2. Gezien door de tijd
+# 3. Zelfde woonplaats medewerker en order plaats
+# 4. Meetbaar datakwaliteit
+# 5. Score opstellen a.d.v. bovenstaande bolletjes
+# 6. Verschil AP/EP (ander personeel/eigen personeel)
+# 
+
+
+
+
 # 1. completeness
 
 # 2. consistency
